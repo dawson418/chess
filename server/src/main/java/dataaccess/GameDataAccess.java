b14e7a1e2c03d83ecd1f62eb61d8bf8e6f20b1e9
Dawson Martin <dawsonjm@byu.edu>
2026-02-26 16:02:05 -0700

    Implemented GameDataAccess interface (not the in-memory implementation yet)

--- server/src/main/java/dataaccess/GameDataAccess.java
+++ server/src/main/java/dataaccess/GameDataAccess.java
@@ -1,4 +1,12 @@
 package dataaccess;
 
+import model.GameData;
+
+import java.util.Collection;
+
 public interface GameDataAccess {
+    GameData createGame() throws DataAccessException;
+    GameData getGame(int gameID) throws DataAccessException;
+    GameData updateGame(int gameID) throws DataAccessException;
+    Collection<GameData> listGames() throws DataAccessException;
 }
