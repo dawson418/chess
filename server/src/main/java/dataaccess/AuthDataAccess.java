bdbe544a5d504b3f77e74f960e4dee78c2cdc443
Dawson Martin <dawsonjm@byu.edu>
2026-02-26 15:58:50 -0700

    Implemented AuthDataAccess interface (not the in-memory implementation yet)

--- server/src/main/java/dataaccess/AuthDataAccess.java
+++ server/src/main/java/dataaccess/AuthDataAccess.java
@@ -1,4 +1,9 @@
 package dataaccess;
 
+import model.AuthData;
+
 public interface AuthDataAccess {
+    AuthData createAuth() throws DataAccessException;
+    AuthData getAuth() throws DataAccessException;
+    AuthData deleteAuth() throws DataAccessException;
 }
