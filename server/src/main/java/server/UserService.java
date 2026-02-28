874ad173ccb8f0a83b3a23790eef9d96c4f13889
Dawson Martin <dawsonjm@byu.edu>
2026-02-27 22:07:42 -0700

    Added LoginRequest, RegisterRequest, LogoutRequest, and LoginResult records. Added UserService class with some of the initial implementation.

---
+++ server/src/main/java/server/UserService.java
@@ -0,0 +1,17 @@
+package server;
+
+import request.LoginRequest;
+import request.LogoutRequest;
+import request.RegisterRequest;
+import result.LoginResult;
+
+public class UserService {
+    public LoginResult register(RegisterRequest registerRequest) {
+        return null;
+    }
+    public LoginResult login(LoginRequest loginRequest) {
+        return null;
+    }
+    public void logout(LogoutRequest logoutRequest) {
+    }
+}
