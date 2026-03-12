8acbd55194f7a982ba2d058a02309f03c0df8b64
Dawson Martin <dawsonjm@byu.edu>
2026-03-11 23:06:36 -0600

    Fixed final overall bugs, now passes all relevant tests

--- server/src/main/java/handler/Handler.java
+++ server/src/main/java/handler/Handler.java
@@ -6,15 +6,17 @@ import dataaccess.BadRequestException;
 import dataaccess.DataAccessException;
 import dataaccess.UnauthorizedException;
 import io.javalin.http.Context;
-import model.AuthData;
 import result.ErrorResult;
 import server.AuthService;
 import server.GameService;
 import server.UserService;
 
+import java.util.Map;
+
 public class Handler {
     public void handleError(Context ctx, Exception e){
         Gson gson = new Gson();
+        String message = e.getMessage();
         ErrorResult result = new ErrorResult(e.getMessage());
         if (e.getClass().equals(BadRequestException.class)){
             ctx.status(400);
@@ -22,11 +24,14 @@ public class Handler {
         else if(e.getClass().equals(UnauthorizedException.class)){
             ctx.status(401);
         }
-        else if(e.getClass().equals(AlreadyTakenException.class)){
+        else if(e.getClass().equals(AlreadyTakenException.class) || message.contains("Duplicate entry")){
             ctx.status(403);
         }
         else{ctx.status(500);}
-        ctx.result(gson.toJson(result));
+        if (message == null || !message.toLowerCase().contains("error")) {
+            message = "Error: " + (message == null ? "internal server error" : message);
+        }
+        ctx.result(new Gson().toJson(Map.of("message", message)));
     }
 
     public void handleClear(Context ctx, AuthService as, GameService gs, UserService us){
