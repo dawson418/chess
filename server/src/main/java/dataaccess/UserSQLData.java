2d0c50f6af1fb018c7ff21b61cc3d8d6ec47b2c7
Dawson Martin <dawsonjm@byu.edu>
2026-03-11 19:39:23 -0600

    Implemented UserSQLData Class

--- server/src/main/java/dataaccess/UserSQLData.java
+++ server/src/main/java/dataaccess/UserSQLData.java
@@ -1,20 +1,48 @@
 package dataaccess;
 
+import com.google.gson.Gson;
 import model.UserData;
 
-public class UserSQLData implements UserDataAccess{
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
+
+public class UserSQLData extends SQLData implements UserDataAccess {
+
+    public UserSQLData() throws DataAccessException {
+        configureDatabase();
+    }
+
     @Override
     public void clear() throws DataAccessException {
-
+        var statement = "TRUNCATE user";
     }
 
     @Override
     public void createUser(UserData data) throws DataAccessException {
-
+        var statement = "INSERT INTO user (username, password, email) VALUES (?, ?, ?)";
+        int id = executeUpdate(statement, data.username(), data.password(), data.email());
     }
 
     @Override
     public UserData getUser(String username, String password) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT username, password, email FROM user WHERE username=?";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.setString(1, username);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return new UserData(rs.getString("username"), rs.getString("password"), rs.getString("email"));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
         return null;
     }
 }
