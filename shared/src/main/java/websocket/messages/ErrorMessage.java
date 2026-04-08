ade11f14577dbe77ef81e6c2ca17ac59ec4408b1
Dawson Martin <dawsonjm@byu.edu>
2026-04-08 15:23:14 -0600

    Created initial message classes

---
+++ shared/src/main/java/websocket/messages/ErrorMessage.java
@@ -0,0 +1,16 @@
+package websocket.messages;
+
+import chess.ChessGame;
+
+public class ErrorMessage extends ServerMessage{
+    private final String errorMessage;
+
+    public ErrorMessage(ServerMessageType type, String errorMessage){
+        super(type);
+        this.errorMessage = errorMessage;
+    }
+
+    public String getMessage(){
+        return errorMessage;
+    }
+}
