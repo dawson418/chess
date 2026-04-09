887ef214718f39f3c4642c8d2baa3b711e88238d
Dawson Martin <dawsonjm@byu.edu>
2026-04-09 17:17:54 -0600

    Updated ChessClient to initiate a websocket connection on join or observe, and created methods to handle parsing of message subclasses

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -1,24 +1,30 @@
 package client;
 
 import chess.ChessGame;
+import com.google.gson.Gson;
 import exception.ResponseException;
 import model.GameData;
 import request.*;
 import ui.BoardUI;
+import websocket.messages.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 import static ui.EscapeSequences.*;
 
-public class ChessClient {
+public class ChessClient implements ServerMessageHandler{
     private String name = null;
     private final ServerFacade server;
+    private final String serverUrl;
     private State state = State.PRELOGIN;
     private String authToken = null;
     private ArrayList<GameData> gameList = new ArrayList<>();
+    private ChessGame.TeamColor playerColor = ChessGame.TeamColor.WHITE;
+    private WebSocketFacade ws;
 
     public ChessClient(String serverUrl) throws ResponseException {
+        this.serverUrl = serverUrl;
         server = new ServerFacade(serverUrl);
     }
 
@@ -46,7 +52,6 @@ public class ChessClient {
     private void printPrompt() {
         System.out.print("\n>>> ");
     }
-
 
     public String eval(String input) {
         try {
@@ -145,6 +150,9 @@ public class ChessClient {
                 throw new ResponseException(400, "Error: Not a valid color");
             }
             server.joinGame(new JoinGameRequest(color, gameID, this.name), this.authToken);
+            playerColor = color;
+            ws = new WebSocketFacade(serverUrl, this);
+            ws.connect(authToken, gameID);
             return new BoardUI(game.getBoard()).drawBoard(color);
         }
         throw new ResponseException(400, "Expected: join <NUMBER> [WHITE|BLACK]");
@@ -165,11 +173,14 @@ public class ChessClient {
                 throw new ResponseException(400, "Please enter a valid game number");
             }
             ChessGame game = gameList.get(i).game();
-            ChessGame.TeamColor color = ChessGame.TeamColor.WHITE;
+            playerColor = ChessGame.TeamColor.WHITE;
             if (params[0].equalsIgnoreCase("BLACK")){
-                color = ChessGame.TeamColor.BLACK;
+                playerColor = ChessGame.TeamColor.BLACK;
             }
-            return new BoardUI(game.getBoard()).drawBoard(color);
+            ws = new WebSocketFacade(this.serverUrl, this);
+            ws.connect(authToken, gameList.get(i).gameID());
+
+            return new BoardUI(game.getBoard()).drawBoard(playerColor);
         }
         throw new ResponseException(400, "Expected: join <NUMBER>");
     }
@@ -198,5 +209,34 @@ public class ChessClient {
         if (state == State.PRELOGIN) {
             throw new ResponseException(400, "You must sign in");
         }
+    }
+
+
+    @Override
+    public void notify(String message) {
+        ServerMessage genMessage = new Gson().fromJson(message, ServerMessage.class);
+        switch (genMessage.getServerMessageType()) {
+            case NOTIFICATION -> handleNotification(message);
+            case ERROR -> handleError(message);
+            case LOAD_GAME -> handleLoadGame(message);
+        }
+    }
+
+    private void handleLoadGame(String message) {
+        LoadGameMessage gameMsg = new Gson().fromJson(message, LoadGameMessage.class);
+        String board = new BoardUI(gameMsg.getGame().getBoard()).drawBoard(this.playerColor);
+        System.out.println("\n" + board);
+        printPrompt();
+    }
+
+    private void handleError(String message) {
+        ErrorMessage error = new Gson().fromJson(message, ErrorMessage.class);
+        System.out.println("\n" + SET_TEXT_COLOR_RED + error.getMessage() + RESET_TEXT_COLOR);
+    }
+
+    private void handleNotification(String message){
+        NotificationMessage notification = new Gson().fromJson(message, NotificationMessage.class);
+        System.out.println("\n" + SET_TEXT_COLOR_BLUE + notification.getMessage() + RESET_TEXT_COLOR);
+        printPrompt();
     }
 }
