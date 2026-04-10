811b025ff31f4ac50578ff90bfd70e225f7f7341
Dawson Martin <dawsonjm@byu.edu>
2026-04-09 18:09:31 -0600

    Updated ConnectionManager with necessary methods for chess, not petshop

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -1,29 +1,48 @@
 package server.websocket;
 
 import org.eclipse.jetty.websocket.api.Session;
-import webSocketMessages.Notification;
 
 import java.io.IOException;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ConnectionManager {
-    public final ConcurrentHashMap<Session, Session> connections = new ConcurrentHashMap<>();
+    public final ConcurrentHashMap<Integer, ConcurrentHashMap<String,Session>> connections = new ConcurrentHashMap<>();
 
-    public void add(Session session) {
-        connections.put(session, session);
+    public void add(int gameID, String authToken, Session session) {
+        connections.putIfAbsent(gameID, new ConcurrentHashMap<>());
+        ConcurrentHashMap<String, Session> gameLobby = connections.get(gameID);
+        gameLobby.put(authToken, session);
     }
 
-    public void remove(Session session) {
-        connections.remove(session);
+    public void remove(int gameID, String authToken) {
+        ConcurrentHashMap<String, Session> gameConnections = connections.get(gameID);
+        if (gameConnections != null){
+            gameConnections.remove(authToken);
+            if (gameConnections.isEmpty()) {
+                connections.remove(gameID);
+            }
+        }
     }
 
-    public void broadcast(Session excludeSession, Notification notification) throws IOException {
-        String msg = notification.toString();
-        for (Session c : connections.values()) {
-            if (c.isOpen()) {
-                if (!c.equals(excludeSession)) {
-                    c.getRemote().sendString(msg);
+    public void broadcast(int gameID, String excludedToken, String msg) throws IOException {
+        ConcurrentHashMap<String, Session> gameConnections = connections.get(gameID);
+        if (gameConnections != null) {
+            for (var entry : gameConnections.entrySet()) {
+                String authToken = entry.getKey();
+                Session session = entry.getValue();
+                if (session.isOpen() && !excludedToken.equals(authToken)){
+                    session.getRemote().sendString(msg);
                 }
+            }
+        }
+    }
+
+    public void soloMessage(int gameID, String targetToken, String msg) throws IOException{
+        ConcurrentHashMap<String, Session> gameConnections = connections.get(gameID);
+        if (gameConnections != null) {
+            Session session = gameConnections.get(targetToken);
+            if (session != null && session.isOpen()){
+                session.getRemote().sendString(msg);
             }
         }
     }
