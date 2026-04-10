566ab58df18b4b91e2ba904fc66bf1de88f67ba1
Dawson Martin <dawsonjm@byu.edu>
2026-04-09 18:09:49 -0600

    Added connect method

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,17 +1,31 @@
 package server.websocket;
 
 import com.google.gson.Gson;
+import dataaccess.*;
 import exception.ResponseException;
 import io.javalin.websocket.*;
+import model.AuthData;
+import model.GameData;
 import org.eclipse.jetty.websocket.api.Session;
-import webSocketMessages.Action;
-import webSocketMessages.Notification;
+import websocket.commands.*;
+import websocket.messages.ErrorMessage;
+import websocket.messages.LoadGameMessage;
+import websocket.messages.NotificationMessage;
 
 import java.io.IOException;
+
+import static websocket.messages.ServerMessage.ServerMessageType.*;
 
 public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
 
     private final ConnectionManager connections = new ConnectionManager();
+    private final AuthDataAccess authDAO;
+    private final GameDataAccess gameDAO;
+
+    public WebSocketHandler(AuthDataAccess authDAO, GameDataAccess gameDAO){
+        this.authDAO = authDAO;
+        this.gameDAO = gameDAO;
+    }
 
     @Override
     public void handleConnect(WsConnectContext ctx) {
@@ -21,43 +35,78 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
 
     @Override
     public void handleMessage(WsMessageContext ctx) {
+        String message = ctx.message();
+        UserGameCommand genCmd = new Gson().fromJson(message, UserGameCommand.class);
         try {
-            Action action = new Gson().fromJson(ctx.message(), Action.class);
-            switch (action.type()) {
-                case ENTER -> enter(action.visitorName(), ctx.session);
-                case EXIT -> exit(action.visitorName(), ctx.session);
+            switch (genCmd.getCommandType()) {
+                case CONNECT-> connect(message, ctx.session);
+                case MAKE_MOVE-> makeMove(message, ctx.session);
+                case LEAVE -> leave(message, ctx.session);
+                case RESIGN -> resign(message, ctx.session);
             }
-        } catch (IOException ex) {
-            ex.printStackTrace();
+        } catch (Exception ex) {
+            try {
+                ErrorMessage error = new ErrorMessage(ERROR,"Error: " + ex.getMessage());
+                ctx.session.getRemote().sendString(new Gson().toJson(error));
+            } catch (IOException e){
+                e.printStackTrace();
+            }
         }
     }
+
+    private void connect(String message, Session session) throws Exception {
+        ConnectCommand cmd = new Gson().fromJson(message, ConnectCommand.class);
+        String username;
+        try {
+            authDAO.isAuthorized(cmd.getAuthToken());
+            username = authDAO.getUsername(cmd.getAuthToken());
+        } catch (DataAccessException e){
+            throw new UnauthorizedException();
+        }
+        GameData gameData;
+        try {
+            gameData = gameDAO.getGame(cmd.getGameID());
+            if (gameData == null){
+                throw new BadRequestException();
+            }
+        } catch (DataAccessException e){
+            throw new BadRequestException();
+        }
+        connections.add(cmd.getGameID(), cmd.getAuthToken(), session);
+
+        LoadGameMessage loadGameMessage = new LoadGameMessage(LOAD_GAME, gameData.game());
+        connections.soloMessage(cmd.getGameID(), cmd.getAuthToken(), new Gson().toJson(loadGameMessage));
+
+        String role = "observer";
+        if (username.equals(gameData.whiteUsername())) {
+            role = "White";
+        } else if (username.equals(gameData.blackUsername())) {
+            role = "Black";
+        }
+
+        String joinMsg = username + "joined the game as" + role;
+        NotificationMessage notification = new NotificationMessage(NOTIFICATION, joinMsg);
+        connections.broadcast(cmd.getGameID(), cmd.getAuthToken(), new Gson().toJson(notification));
+    }
+
+    private void makeMove(String message, Session session) throws IOException {
+        MakeMoveCommand cmd = new Gson().fromJson(message, MakeMoveCommand.class);
+    }
+
+    private void leave(String message, Session session) throws IOException {
+        LeaveCommand cmd = new Gson().fromJson(message, LeaveCommand.class);
+        connections.remove(cmd.getGameID(), cmd.getAuthToken());
+    }
+
+    private void resign(String message, Session session) throws IOException {
+        ResignCommand cmd = new Gson().fromJson(message, ResignCommand.class);
+    }
+
 
     @Override
     public void handleClose(WsCloseContext ctx) {
         System.out.println("Websocket closed");
     }
 
-    private void enter(String visitorName, Session session) throws IOException {
-        connections.add(session);
-        var message = String.format("%s is in the shop", visitorName);
-        var notification = new Notification(Notification.Type.ARRIVAL, message);
-        connections.broadcast(session, notification);
-    }
 
-    private void exit(String visitorName, Session session) throws IOException {
-        var message = String.format("%s left the shop", visitorName);
-        var notification = new Notification(Notification.Type.DEPARTURE, message);
-        connections.broadcast(session, notification);
-        connections.remove(session);
-    }
-
-    public void makeNoise(String petName, String sound) throws ResponseException {
-        try {
-            var message = String.format("%s says %s", petName, sound);
-            var notification = new Notification(Notification.Type.NOISE, message);
-            connections.broadcast(null, notification);
-        } catch (Exception ex) {
-            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
-        }
-    }
 }
